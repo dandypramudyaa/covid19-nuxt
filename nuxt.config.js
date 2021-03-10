06ef29201bb67eaccad01e34306b403c6f59792d
dandyp <dandyp@thebigrichgroup.com>
2021-03-11 06:12:44 +0700

    testing v.1.1

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    '@nuxtjs/moment',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
