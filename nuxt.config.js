15e5a12bcd60531ff21378e016812eda792e80ef
dandyp <dandyp@thebigrichgroup.com>
2021-03-10 18:06:44 +0700

    fix 18

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,8 +46,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
-  devModules: ['@nuxtjs/axios'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
